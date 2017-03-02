53debe4b193b02ef328c62b55b5267199aed0e31
Garion Herman <garion@silverstripe.com>
2017-03-02 22:42:27 +1300

    Fix link typo, set app title.

--- pages/board/_id/thread/_id.vue
+++ pages/board/_id/thread/_id.vue
@@ -4,7 +4,7 @@
       <div class="heading">
         <h6 class="subtitle is-6">
           <nuxt-link to="/boards">Boards</nuxt-link> &mdash;
-          <nuxt-link :to="`/boards/${board.ID}`">{{ board.Name }}</nuxt-link> &mdash;
+          <nuxt-link :to="`/board/${board.ID}`">{{ board.Name }}</nuxt-link> &mdash;
         </h6>
         <h1 class="title is-1">{{ name }}</h1>
       </div>
@@ -16,12 +16,26 @@
         <p><em>&mdash;{{ post.Author }}</em></p>
       </div>
     </section>
+
+    <section class="section">
+      <h4 class="title is-4">Reply</h4>
+
+      <p class="control">
+        <textarea v-model="draftMessage"
+          class="textarea"
+          placeholder="Message">
+        </textarea>
+      </p>
+
+      <button class="button is-primary" @click.prevent="addComment">Send</button>
+    </section>
   </div>
 </template>
 
 <script>
   import api from '~plugins/api'
 
+  // TODO: Shift this component to using state properly
   export default {
     data ({ params }) {
       return api.get(`/api/v1/thread/${params.id}`)
@@ -30,7 +44,9 @@
             id: params.id,
             name: res.data.Name,
             board: res.data.Board,
-            posts: res.data.Posts
+            posts: res.data.Posts,
+
+            draftMessage: ''
           }
         })
     },
@@ -40,6 +56,22 @@
           .then((res) => {
             this.posts = res.data.Posts
           })
+      },
+      addComment () {
+        if (this.draftMessage === '') return
+
+        return api({
+          method: 'post',
+          url: `/api/v1/thread/${this.id}/reply`,
+          data: {
+            author: this.$store.state.user.name,
+            message: this.draftMessage
+          }
+        }).then((res) => {
+          this.draftMessage = ''
+
+          this.posts.push(res.data)
+        })
       }
     }
   }
